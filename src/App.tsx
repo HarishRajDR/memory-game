d3f44807c9424191b8b0df3f38f2f622c651bc82
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-21 19:41:00 +0530

    customised the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello There!</h1>
+      <h1 className="text-2xl">Hello There!</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(199, 72%, 67%)",
+        secondary: "hsl(200, 71%, 15%)",
+        accent: "hsl(199, 72%, 55%)",
+      },
+    },
   },
   plugins: [],
 };
