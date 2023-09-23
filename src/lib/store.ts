4fc1514ac0e3dbd0056b604d4295b9cc7ba2c0aa
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-23 11:28:06 +0530

    added all 12 card's state and functions

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -5,6 +5,15 @@ type State = {
   one: boolean;
   two: boolean;
   three: boolean;
+  four: boolean;
+  five: boolean;
+  six: boolean;
+  seven: boolean;
+  eight: boolean;
+  nine: boolean;
+  ten: boolean;
+  eleven: boolean;
+  tweleve: boolean;
 };
 
 type Action = {
@@ -12,6 +21,15 @@ type Action = {
   updateOne: (p: State["one"]) => void;
   updateTwo: (p: State["two"]) => void;
   updateThree: (p: State["three"]) => void;
+  updateFour: (p: State["four"]) => void;
+  updateFive: (p: State["five"]) => void;
+  updateSix: (p: State["six"]) => void;
+  updateSeven: (p: State["seven"]) => void;
+  updateEight: (p: State["eight"]) => void;
+  updateNine: (p: State["nine"]) => void;
+  updateTen: (p: State["ten"]) => void;
+  updateEleven: (p: State["eleven"]) => void;
+  updateTweleve: (p: State["tweleve"]) => void;
   reset: () => void;
 };
 
@@ -20,6 +38,15 @@ const initialValues: State = {
   one: false,
   two: false,
   three: false,
+  four: false,
+  five: false,
+  six: false,
+  seven: false,
+  eight: false,
+  nine: false,
+  ten: false,
+  eleven: false,
+  tweleve: false,
 };
 
 export const useBoardStore = create<State & Action>((set, get) => ({
@@ -28,5 +55,15 @@ export const useBoardStore = create<State & Action>((set, get) => ({
   updateOne: (p) => set(() => ({ one: p })),
   updateTwo: (p) => set(() => ({ two: p })),
   updateThree: (p) => set(() => ({ three: p })),
+  updateFour: (p) => set(() => ({ four: p })),
+  updateFive: (p) => set(() => ({ five: p })),
+  updateSix: (p) => set(() => ({ six: p })),
+  updateSeven: (p) => set(() => ({ seven: p })),
+  updateEight: (p) => set(() => ({ eight: p })),
+  updateNine: (p) => set(() => ({ nine: p })),
+  updateTen: (p) => set(() => ({ ten: p })),
+  updateEleven: (p) => set(() => ({ eleven: p })),
+  updateTweleve: (p) => set(() => ({ tweleve: p })),
+
   reset: () => set(initialValues),
 }));
