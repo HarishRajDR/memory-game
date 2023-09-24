3dd05fdcabd1471574219393f52f92457f1a250b
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-24 09:57:05 +0530

    added logic for reseting only incorrect cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,11 +3,22 @@ import { gridGenerate } from "../lib/grid";
 import { useBoardStore } from "../lib/store";
 import Symbol from "./Symbol";
 
+type selected = {
+  gridNo: number | null;
+  contents: number | null;
+};
+
 const grid = gridGenerate();
 
 function Board() {
-  const [selected1, setSelected1] = useState<number | null>(null);
-  const [selected2, setSelected2] = useState<number | null>(null);
+  const [selected1, setSelected1] = useState<selected>({
+    gridNo: null,
+    contents: null,
+  });
+  const [selected2, setSelected2] = useState<selected>({
+    gridNo: null,
+    contents: null,
+  });
 
   const turns = useBoardStore((state) => state.turns);
   const updateTurns = useBoardStore((state) => state.updateTurn);
@@ -17,50 +28,74 @@ function Board() {
     {
       stateVariable: useBoardStore((state) => state.one),
       stateFunction: useBoardStore((state) => state.updateOne),
+      stateCorrect: useBoardStore((state) => state.isoneCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsOneCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.two),
       stateFunction: useBoardStore((state) => state.updateTwo),
+      stateCorrect: useBoardStore((state) => state.istwoCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsTwoCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.three),
       stateFunction: useBoardStore((state) => state.updateThree),
+      stateCorrect: useBoardStore((state) => state.isthreeCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsThreeCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.four),
       stateFunction: useBoardStore((state) => state.updateFour),
+      stateCorrect: useBoardStore((state) => state.isfourCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsFourCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.five),
       stateFunction: useBoardStore((state) => state.updateFive),
+      stateCorrect: useBoardStore((state) => state.isfiveCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsFiveCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.six),
       stateFunction: useBoardStore((state) => state.updateSix),
+      stateCorrect: useBoardStore((state) => state.issixCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsSixCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.seven),
       stateFunction: useBoardStore((state) => state.updateSeven),
+      stateCorrect: useBoardStore((state) => state.issevenCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsSevenCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.eight),
       stateFunction: useBoardStore((state) => state.updateEight),
+      stateCorrect: useBoardStore((state) => state.iseightCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsEightCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.nine),
       stateFunction: useBoardStore((state) => state.updateNine),
+      stateCorrect: useBoardStore((state) => state.isnineCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsNineCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.ten),
       stateFunction: useBoardStore((state) => state.updateTen),
+      stateCorrect: useBoardStore((state) => state.istenCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsTenCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.eleven),
       stateFunction: useBoardStore((state) => state.updateEleven),
+      stateCorrect: useBoardStore((state) => state.iselevenCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsElevenCorrect),
     },
     {
       stateVariable: useBoardStore((state) => state.tweleve),
       stateFunction: useBoardStore((state) => state.updateTweleve),
+      stateCorrect: useBoardStore((state) => state.istweleveCorrect),
+      stateCFunction: useBoardStore((state) => state.updateIsTweleveCorrect),
     },
   ];
 
@@ -70,7 +105,7 @@ function Board() {
     check();
   }, [turns]);
 
-  function update(h: number) {
+  function update(h: selected) {
     if (turns == 0) {
       setSelected1(h);
     }
@@ -81,14 +116,26 @@ function Board() {
 
   function check() {
     if (turns == 2) {
-      if (selected1 == selected2) {
+      if (
+        selected1.contents == selected2.contents &&
+        selected1.gridNo != null &&
+        selected2.gridNo != null
+      ) {
+        boardData[selected1.gridNo].stateCFunction();
+        boardData[selected2.gridNo].stateCFunction();
         resetTurns();
       } else
         setTimeout(() => {
           reset();
-          setSelected1(null);
-          setSelected2(null);
-        }, 200);
+          setSelected1({
+            gridNo: null,
+            contents: null,
+          });
+          setSelected2({
+            gridNo: null,
+            contents: null,
+          });
+        }, 2000);
     }
   }
 
@@ -100,11 +147,15 @@ function Board() {
           onClick={() => {
             updateTurns();
             boardData[idx].stateFunction(true);
-            update(p);
+            const h = {
+              gridNo: idx,
+              contents: p,
+            };
+            update(h);
           }}
-          disabled={turns == 2}
+          disabled={turns == 2 || boardData[idx].stateCorrect}
           className={
-            boardData[idx].stateVariable
+            boardData[idx].stateVariable || boardData[idx].stateCorrect
               ? "bg-secondary w-40 aspect-[3/4] rounded-md flex items-center justify-center"
               : "bg-primary w-40 aspect-[3/4] rounded-md flex items-center justify-center"
           }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,38 +3,75 @@ import { create } from "zustand";
 type State = {
   turns: number;
   one: boolean;
+  isoneCorrect: boolean;
   two: boolean;
+  istwoCorrect: boolean;
   three: boolean;
+  isthreeCorrect: boolean;
   four: boolean;
+  isfourCorrect: boolean;
   five: boolean;
+  isfiveCorrect: boolean;
   six: boolean;
+  issixCorrect: boolean;
   seven: boolean;
+  issevenCorrect: boolean;
   eight: boolean;
+  iseightCorrect: boolean;
   nine: boolean;
+  isnineCorrect: boolean;
   ten: boolean;
+  istenCorrect: boolean;
   eleven: boolean;
+  iselevenCorrect: boolean;
   tweleve: boolean;
+  istweleveCorrect: boolean;
 };
 
 type Action = {
   updateTurn: () => void;
   resetTurn: () => void;
+
   updateOne: (p: State["one"]) => void;
+  updateIsOneCorrect: () => void;
+
   updateTwo: (p: State["two"]) => void;
+  updateIsTwoCorrect: () => void;
+
   updateThree: (p: State["three"]) => void;
+  updateIsThreeCorrect: () => void;
+
   updateFour: (p: State["four"]) => void;
+  updateIsFourCorrect: () => void;
+
   updateFive: (p: State["five"]) => void;
+  updateIsFiveCorrect: () => void;
+
   updateSix: (p: State["six"]) => void;
+  updateIsSixCorrect: () => void;
+
   updateSeven: (p: State["seven"]) => void;
+  updateIsSevenCorrect: () => void;
+
   updateEight: (p: State["eight"]) => void;
+  updateIsEightCorrect: () => void;
+
   updateNine: (p: State["nine"]) => void;
+  updateIsNineCorrect: () => void;
+
   updateTen: (p: State["ten"]) => void;
+  updateIsTenCorrect: () => void;
+
   updateEleven: (p: State["eleven"]) => void;
+  updateIsElevenCorrect: () => void;
+
   updateTweleve: (p: State["tweleve"]) => void;
+  updateIsTweleveCorrect: () => void;
+
   reset: () => void;
 };
 
-const initialValues: State = {
+const initialValues = {
   turns: 0,
   one: false,
   two: false,
@@ -52,20 +89,56 @@ const initialValues: State = {
 
 export const useBoardStore = create<State & Action>((set, get) => ({
   ...initialValues,
+  isoneCorrect: false,
+  istwoCorrect: false,
+  isthreeCorrect: false,
+  isfourCorrect: false,
+  isfiveCorrect: false,
+  issixCorrect: false,
+  issevenCorrect: false,
+  iseightCorrect: false,
+  isnineCorrect: false,
+  istenCorrect: false,
+  iselevenCorrect: false,
+  istweleveCorrect: false,
   updateTurn: () => set(() => ({ turns: get().turns + 1 })),
   resetTurn: () => set(() => ({ turns: 0 })),
+
   updateOne: (p) => set(() => ({ one: p })),
+  updateIsOneCorrect: () => set(() => ({ isoneCorrect: true })),
+
   updateTwo: (p) => set(() => ({ two: p })),
+  updateIsTwoCorrect: () => set(() => ({ istwoCorrect: true })),
+
   updateThree: (p) => set(() => ({ three: p })),
+  updateIsThreeCorrect: () => set(() => ({ isthreeCorrect: true })),
+
   updateFour: (p) => set(() => ({ four: p })),
+  updateIsFourCorrect: () => set(() => ({ isfourCorrect: true })),
+
   updateFive: (p) => set(() => ({ five: p })),
+  updateIsFiveCorrect: () => set(() => ({ isfiveCorrect: true })),
+
   updateSix: (p) => set(() => ({ six: p })),
+  updateIsSixCorrect: () => set(() => ({ issixCorrect: true })),
+
   updateSeven: (p) => set(() => ({ seven: p })),
+  updateIsSevenCorrect: () => set(() => ({ issevenCorrect: true })),
+
   updateEight: (p) => set(() => ({ eight: p })),
+  updateIsEightCorrect: () => set(() => ({ iseightCorrect: true })),
+
   updateNine: (p) => set(() => ({ nine: p })),
+  updateIsNineCorrect: () => set(() => ({ isnineCorrect: true })),
+
   updateTen: (p) => set(() => ({ ten: p })),
+  updateIsTenCorrect: () => set(() => ({ istenCorrect: true })),
+
   updateEleven: (p) => set(() => ({ eleven: p })),
+  updateIsElevenCorrect: () => set(() => ({ iselevenCorrect: true })),
+
   updateTweleve: (p) => set(() => ({ tweleve: p })),
+  updateIsTweleveCorrect: () => set(() => ({ istweleveCorrect: true })),
 
   reset: () => set(initialValues),
 }));
