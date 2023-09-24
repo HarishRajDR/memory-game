2ae65cdc65ef7f10f30a4683aaa6104df2887133
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-24 10:07:40 +0530

    added Bricolage Grotesque font for heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        bricolageGrotesque: ["Bricolage Grotesque", "san-serif"],
+      },
       colors: {
         primary: "hsl(199, 72%, 67%)",
         secondary: "hsl(200, 71%, 15%)",
