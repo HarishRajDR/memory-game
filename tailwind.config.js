0a776ac7ab638ed48cf44b328314c12edfb3555d
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-29 10:49:56 +0530

    added text and background colors in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         primary: "hsl(199, 72%, 67%)",
         secondary: "hsl(200, 71%, 15%)",
         accent: "hsl(199, 72%, 55%)",
+        text: "hsl(199, 73%, 94%)",
+        background: "hsl(198, 72%, 7%)",
       },
     },
   },
