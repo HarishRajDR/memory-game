8dd232fa03a07bc97ae2b4c826e32bfa47c75043
Harish Raj D R <drharishrajdr@gmail.com>
2023-09-29 11:58:36 +0530

    added footer, favicon and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Board from "./components/Board";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex items-center justify-center h-screen">
       <Board />
+      <div className="absolute bottom-5 right-5">
+        <Footer />
+      </div>
     </div>
   );
 }
